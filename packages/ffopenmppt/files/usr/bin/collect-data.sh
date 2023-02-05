ea42e85e893d0657db803764abf4e53e69eac74d
hiurequeiroz <hiure@riseup.net>
2023-02-05 17:48:25 +0530

    WIP:adding comments to understand what need to do

--- packages/ffopenmppt/files/usr/bin/collect-data.sh
+++ packages/ffopenmppt/files/usr/bin/collect-data.sh
@@ -6,12 +6,18 @@ mkdir -p /tmp/ffomppt
 while (true); do
 
 wget -q http://hiure-test/csv.log -O /tmp/ffomppt/ffomppt.csv
-pid=$!                                                         
-sleep 60                                                                                  
-lua /usr/lib/lua/ffomppt/main.lua 
-kill $pid ;
+# TODO
+# using hiure-test how exemple but need to automatize utilising wget in each clinte and see a unic pattern that came from omppt
+
+# just to check
+# I dont know if need this pid thing, let here for a while just to remember
+# pid=$!                                                         
+# sleep 60                                                                                  
+# lua /usr/lib/lua/ffomppt/main.lua 
+# kill $pid ;
 
 
+# This is for take care with the size of the file
 LC=`wc -l /tmp/ffomppt/ffomppt.csv | cut -d \  -f 1`
 
 if [ $LC -ge 3660 ]
